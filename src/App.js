7b35f8fcc7f116546f550048f11c1b58435ad1cd
Alejandro Ruzafa <ruzafa-@outlook.es>
2022-12-25 19:44:31 +0100

    resolve conflixa with master

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-=======
-import logo from './logo.svg';
->>>>>>> master
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <p>Working on it!</p>
       </header>
       <nav></nav>
@@ -17,21 +12,6 @@ function App() {
       </section>
       <aside></aside>
       <footer></footer>
-=======
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> master
     </div>
   );
 }
