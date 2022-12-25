eeff2ef26893d644e70f64589addc9d8034cd13c
ruzafa8 <34567477+ruzafa8@users.noreply.github.com>
2022-12-25 23:33:04 +0100

    Simple Welcome (#2)
    * Init react app
    
    Default files generated by npx cli.
    
    * gh-pages dependency added
    
    So that git hub pages can build the files and deploy them
    
    * config gh-pages
    
    * set build branch
    
    * Readme update
    
    * Add font style
    
    * Simple message
    
    * normalize css
    
    * improve html structure
    
    * resolver conflicts with master
    
    * resolve conflixa with master
    
    * redo tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,6 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik+80s+Fade&display=swap" rel="stylesheet"></link>
     <App />
   </React.StrictMode>
 );
