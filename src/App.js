d92ac990e73b2490dda62568b4a20b32eaaaa2e2
Alejandro Ruzafa <ruzafa-@outlook.es>
2022-12-25 14:02:53 +0100

    improve html structure

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Welcome to
-        </p>
-        <p>
-           my space!
-        </p>
+        <p>Working on it!</p>
       </header>
+      <nav></nav>
+      <section>
+        <h1>Welcome to <br/> my space!</h1>
+      </section>
+      <aside></aside>
+      <footer></footer>
     </div>
   );
 }
